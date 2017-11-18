04187916ea1790abcc581abc6afddde43eddea4c
Yoël Cornamusaz <iioel64@gmail.com>
2017-11-18 17:33:48 +0100

    Add view home (file/)

--- file_store/urls.py
+++ file_store/urls.py
@@ -1,4 +1,4 @@
-"""liftoff URL Configuration
+"""file_store URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views as file_store
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^file/', include('file_store.urls')),
-    url(r'^tags_input/', include('tags_input.urls', namespace='tags_input')),
+    url(r'^$', file_store.home, name='home'),
 ]
