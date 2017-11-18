854d5bb973c7cf37097dd04938deed44511e8c66
Yoël Cornamusaz <iioel64@gmail.com>
2017-11-18 16:22:35 +0100

    Add module tags_input

--- liftoff/urls.py
+++ liftoff/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tags_input/', include('tags_input.urls', namespace='tags_input')),
 ]
